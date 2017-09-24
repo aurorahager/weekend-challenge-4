4825d16456cd6baf428b6f9a87abc562aee1e08a
Aurora Hager <roryy18@gmail.com>
2017-09-24 11:37:17 -0500

    desc/photo toggle button added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,5 +3,9 @@ var myApp = angular.module( 'myApp', [] );
 
 myApp.controller('GalleryController', function () {
     console.log('ng');
-    
+    var vm = this;
+     vm.favPhoto = function () {
+        console.log('liked!');
+        
+     }
 })//END myApp.controller
